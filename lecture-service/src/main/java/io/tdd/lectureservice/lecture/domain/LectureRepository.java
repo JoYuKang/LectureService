21086ca5edc4a0b5f90f84aaa6736fd98ff5a42b
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2024-12-28 14:57:16 +0900

    refactor: Lock 쿼리 변경

--- lecture-service/src/main/java/io/tdd/lectureservice/lecture/domain/LectureRepository.java
+++ lecture-service/src/main/java/io/tdd/lectureservice/lecture/domain/LectureRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT l FROM Lecture l WHERE l.lectureDate BETWEEN :startDate AND :endDate AND l.capacity != 0")
     List<Lecture> findAllByLectureDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Lecture> findById(Long lectureId);
+
 }
